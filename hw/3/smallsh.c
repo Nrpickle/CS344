71338ae32767ccdd9c7bd388226d7850e2381293
Nick <nick@nickmccomb.net>
2016-02-28 18:08:21 -0800

    Added initial commit of program 3
    Contains mostly working code. Needs some final tweaking (and
    documentation) but it's most of the way there.
    
    Notably lacking:
    - Displays exit status of fg programs
    - Doesn't implement fg to put bg programs into fg

---
+++ hw/3/smallsh.c
@@ -0,0 +1,247 @@
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+#include <signal.h>
+#include <unistd.h>
+#include <fcntl.h>
+#include <sys/wait.h>
+#include <sys/types.h>
+#include <sys/stat.h>
+
+#define USER_INPUT_MAX 2048
+#define USER_NUM_ENTRIES 513
+
+void signalSIGINTHandler(int signum);
+
+int main(){
+  //**PROGRAM INIT**
+  char userInput[USER_INPUT_MAX];
+
+  static int status = -3;
+  
+  //Register signal handlers
+  signal(SIGINT, signalSIGINTHandler);
+
+  printf("[Welcome to Small Shell!] \n");
+  
+  do{
+
+
+    pid_t cpid;
+
+    short timeout = 0;
+    short rep = 0;
+
+    do{
+    do{
+      cpid = waitpid(-1, &status, WNOHANG);
+      if(cpid > 1){
+        printf("%d exited with exit value %i\n", cpid, status);
+      }
+      ++timeout;
+      //printf(".");
+      //fflush(stdout);
+    }while(!cpid && timeout < 1000);
+    }while(rep++ < 5);
+
+
+    printf(": ");
+    fflush(stdout);
+
+    fgets(userInput, USER_INPUT_MAX, stdin);
+     
+    //Strip the newline character
+    userInput[strlen(userInput)-1] = '\0';
+
+    //printf("I recieved the following: %s\n", userInput);
+ 
+    
+    //printf("characterCheck: %c \n", userInput[1]);
+    
+    //Check for a blank line first
+
+    //Check for blank line
+    short inputLen = strlen(userInput);
+    char  isBlank = 1; //Assume the line is blank
+    for (int i = 0; i < inputLen; ++i){
+      if(userInput[i] != ' ' && userInput[i] != '\0'){
+	isBlank = 0;
+	break;
+      }
+    }
+    if(isBlank){
+      continue;
+    }
+
+
+    //We want to get the first space deliminated substring
+    //To do this we will first split it into a vector similar
+    //to argv using strtok
+
+    char  * tokResult = strtok(userInput, " "); 
+    char  ** userInputVect = malloc(USER_NUM_ENTRIES * sizeof(char*));
+    short userInputCount = 0;
+
+    do{
+      //printf("Length of strtok: %d\n", strlen(tokResult));
+
+      userInputVect[userInputCount] = malloc( (strlen(tokResult) + 1) * sizeof(char) );
+      strcpy( userInputVect[userInputCount++] , tokResult );
+
+      //printf("Recieved: %s \n", userInputVect[userInputCount - 1]);
+
+    }while(tokResult = strtok(NULL, " "));
+
+    //userInputVect[userInputCount] = malloc( sizeof(char) * 2);
+    userInputVect[userInputCount] = NULL;
+
+    //Check for built in command
+    if(!strcmp(userInputVect[0], "exit"))
+      exit(0);
+    if(!strcmp(userInputVect[0], "cd")){
+      //printf("[#Executing change dir#]\n");
+      chdir(userInputVect[1]);
+      continue;
+    }
+    if(!strcmp(userInputVect[0], "status")){
+      //printf("[#Executing status#]\n"); 
+      printf("exit value %d \n", status);
+      continue;
+    }
+
+    //Check for comment line
+    if(userInputVect[0][0] == '#'){
+       //Then we want to do nothing
+       continue;
+    }
+
+    //Check to see if we are piping the process anything
+    for(int i = 0; i < userInputCount; i++){
+      if(!strcmp(userInputVect[i], "|")){
+        printf("  #Detected pipe\n");
+      }
+      //printf("Counted input: %d \n", userInputCount);
+    }
+
+    char setBackground = 0;
+    //Check for background process
+    for (int k = 0; k < userInputCount; k++){
+      if(!strcmp(userInputVect[k], "&")){
+        setBackground = 1;
+	free(userInputVect[k]);
+	userInputVect[k] = NULL;  //End the vector earlier (we don't want to pass the & to the exec
+	userInputCount = k - 1;   //We do the same to the count, so the program doesn't iterate to NULL
+	//printf(" ##background this process \n");
+      }
+    }
+
+    //Check for file redirectionm
+    char * outputFilename = NULL;
+    for(int j = 0; j < userInputCount; j++){
+      if(!strcmp(userInputVect[j], ">")){
+        //printf("  #Writing to file! Seperator located at %d \n", j);
+	//put the filename in the output name, also makes the name pointer != zero
+        outputFilename = malloc(sizeof(char) * strlen(userInputVect[j + 1]));
+	strcpy(outputFilename, userInputVect[j + 1]);
+	//Cut the input vector short so that we only output the command to be output to file
+	free(userInputVect[j]); //because otherwise we'd lose this memory
+        userInputVect[j] = NULL;
+	userInputCount = j - 1;
+	break; //Break out of the for loop
+      }
+    }
+
+    char * inputFilename = NULL;
+    for(int lol = 0; lol < userInputCount; lol++){
+      if(!strcmp(userInputVect[lol], "<")){//Then we need to do some stuff and things (read from a file)
+	printf("  #READING FROM FILE \n");
+	inputFilename = malloc(sizeof(char) * strlen(userInputVect[lol + 1]));
+	strcpy(inputFilename, userInputVect[lol + 1]);
+
+	free(userInputVect[lol]);
+	userInputVect[lol] = NULL;
+	userInputCount = lol - 1;
+        break;
+      }
+    }
+
+    //pid_t cpid;
+    //static int status = -1;
+
+    /*short timeout = 0;
+    short rep = 0;
+
+    do{
+    do{
+      cpid = waitpid(-1, &status, 0);
+      if(cpid > 1){
+        printf("exit value %i\n", status);
+      }
+      ++timeout;
+      printf(".");
+      fflush(stdout);
+    }while(!cpid && timeout < 1000);
+    }while(rep++ < 5);
+    */
+    cpid = fork();
+
+    if (cpid == 0){  //Then we are the child
+       int fd;
+
+      if(setBackground){  //Then we have a background process on our hands
+        int null = open("/dev/null", O_RDWR);
+
+        int readNull = open("/dev/null", O_RDONLY);
+
+	//printf("null: %d | readNull: %d \n", null, readNull);
+        //fflush(stdout);
+
+        dup2(null, 1);
+	dup2(readNull, 0);
+	dup2(null, 2);
+
+	//TODO: disable ctrl sequences
+
+      }
+      else if(inputFilename){ 
+        fd = open( inputFilename, O_RDONLY, 0644);
+        
+	dup2(fd, 0);
+
+      }
+      //Check if we need to change the output to a file
+      else if(outputFilename){  //Check if the filename pointer is not null (only if not a background) TODO: fix, bug?
+        fd = open( outputFilename, O_WRONLY | O_CREAT | O_TRUNC, 0644);
+
+        dup2(fd , 1);
+	
+      }
+
+      execvp(userInputVect[0], userInputVect);
+
+      //printf("Failed stuffs");
+      fprintf(stderr, "smallsh: cannot open %s for input\n", userInputVect[0]);
+      
+      //fputs("smallsh: cannot open for input. \n", stderr);
+      exit(1);
+    }
+    else{  //Then we are the parent.
+      if(setBackground)
+        printf("background pid is %d \n", cpid);
+      else{
+        cpid = waitpid(-1, &status, 0);
+      }
+    }
+
+  }while(1);
+}
+
+void signalSIGINTHandler(int signum){
+  printf("terminated by signal %d\n", signum);
+
+  //char error [] = "Caught SIGINT\n";
+  //puts(error);
+
+  //Setup signal handler again
+  signal(SIGINT, signalSIGINTHandler);
+}
