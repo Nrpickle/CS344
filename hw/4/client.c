ffd40d56ef94fadf4ae20c74508371725256e1db
Nick McComb <nrpickle@gmail.com>
2016-03-14 12:02:03 -0700

    Process forks, added decrypt switch

--- hw/4/client.c
+++ hw/4/client.c
@@ -9,70 +9,102 @@
 
 void error(const char *msg)
 {
-    perror(msg);
-    exit(0);
+  perror(msg);
+  exit(0);
 }
 
 int main(int argc, char *argv[])
 {
-    int sockfd, portno, n;
-    struct sockaddr_in serv_addr;
-    struct hostent *server;
+  int sockfd, portno, n;
+  struct sockaddr_in serv_addr;
+  struct hostent *server;
 
-    //Input checking
-    char buffer[256];
-    if (argc < 2) {
-       fprintf(stderr,"usage %s hostname port\n", argv[0]);
-       exit(0);
-    }
-
-    
-    //Open the socket connection to the server
-    portno = atoi(argv[1]);
-    sockfd = socket(AF_INET, SOCK_STREAM, 0);
-    if (sockfd < 0) 
-        error("ERROR opening socket");
-    
-    
-    //error("socket");
-
-    server = gethostbyname("localhost");
-    if (server == NULL) {
-        fprintf(stderr,"ERROR, no such host\n");
-        exit(0);
-    }
-    bzero((char *) &serv_addr, sizeof(serv_addr));
-    serv_addr.sin_family = AF_INET;
-    bcopy((char *)server->h_addr, 
-         (char *)&serv_addr.sin_addr.s_addr,
-         server->h_length);
-    serv_addr.sin_port = htons(portno);
-
-    //error("fail before connect");
-
-    if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
-        error("ERROR connecting");
-    //At this point, we can assume the connection was successful
-    
-    printf("connected to server");
+  //Input checking
+  char buffer[256];
+  if (argc < 2) {
+    fprintf(stderr,"usage %s hostname port\n", argv[0]);
+    exit(0);
+  }
 
 
-    //Get the message to send
-    printf("Please enter the message: ");
-    bzero(buffer,256);
-    fgets(buffer,255,stdin);
+  //Open the socket connection to the server
+  portno = atoi(argv[3]);
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) 
+    error("ERROR opening socket");
 
-    //Write to the server
-    n = write(sockfd,buffer,strlen(buffer));
-    if (n < 0) 
-         error("ERROR writing to socket");
-    bzero(buffer,256);
-    n = read(sockfd,buffer,255);
-    if (n < 0) 
-         error("ERROR reading from socket");
-    printf("%s\n",buffer);
-    close(sockfd);
-    return 0;
+  server = gethostbyname("localhost");
+  if (server == NULL) {
+    fprintf(stderr,"ERROR, no such host\n");
+    exit(0);
+  }
+  bzero((char *) &serv_addr, sizeof(serv_addr));
+  serv_addr.sin_family = AF_INET;
+  bcopy((char *)server->h_addr, 
+      (char *)&serv_addr.sin_addr.s_addr,
+      server->h_length);
+  serv_addr.sin_port = htons(portno);
+
+  //error("fail before connect");
+
+  if (connect(sockfd,(struct sockaddr *) &serv_addr,sizeof(serv_addr)) < 0) 
+    error("ERROR connecting");
+  //At this point, we can assume the connection was successful
+
+  //Load the plaintext and key from files
+
+  FILE *plaintextFile;
+  FILE *keyFile;
+
+  plaintextFile = fopen(argv[1], "r");
+  keyFile = fopen(argv[2], "r");
+
+  if(!(plaintextFile && keyFile))
+    error("[! Cannot open file !]");
+
+  //Get the message to send
+  //fgets(buffer,255,stdin);
+
+  //At this point we can assume we have valid files and a valid socket
+
+  char inputBuffer[2000];
+  char plaintext[2000];
+  char key[2000];
+
+  fgets(inputBuffer, 1000, plaintextFile);
+
+  //I want to strip the newline off if there is one
+  int plaintextLen = strlen(inputBuffer);
+  if(inputBuffer[plaintextLen-1] == '\n')
+    inputBuffer[plaintextLen-1] = '\0';
+
+  //printf("\n\nClient input\nPlaintext file: %s\n", inputBuffer);
+
+  strcpy(plaintext, inputBuffer);
+
+  fgets(inputBuffer, 1000, keyFile);
+  //printf("Keyfile       : %s\n\n", inputBuffer);
+
+  strcpy(key,       inputBuffer);
+
+  //strcpy(plaintext, argv[1]);
+  //strcpy(key,       argv[2]);
+
+  //Write to the server
+  //TODO: Implement sending in chunks of 1000
+  bzero(buffer, 256);
+  n = write(sockfd, plaintext, strlen(plaintext));
+  if (n < 0)
+    error("ERROR writing to socket");
+
+  n = write(sockfd, key, strlen(key));
+  if (n < 0)
+    error("[! ERROR writing to socket !]");
+
+  n = read(sockfd,buffer,255);
+  if (n < 0) 
+    error("ERROR reading from socket");
+  printf("%s\n",buffer);
+  close(sockfd);
+  return 0;
 }
-
-
--- hw/4/server.c
+++ hw/4/server.c
@@ -8,6 +8,10 @@
 #include <sys/socket.h>
 #include <netinet/in.h>
 
+#define ENCRYPT
+
+char convert(char input);
+
 void error(const char *msg)
 {
   perror(msg);
@@ -19,6 +23,8 @@ int main(int argc, char *argv[])
   int sockfd, newsockfd, portno;
   socklen_t clilen;
   char buffer[256];
+  char plaintext[256];
+  char key[256];
   struct sockaddr_in serv_addr, cli_addr;
   int n;
   if (argc < 2) {
@@ -41,22 +47,117 @@ int main(int argc, char *argv[])
     listen(sockfd,5); //Listen to the port
 
     //At this point, we have a valid queued connection
+    //
+    //TODO: Insert fork to enable concurrency
+
     clilen = sizeof(cli_addr);
     newsockfd = accept(sockfd, 
-	(struct sockaddr *) &cli_addr, 
+	(struct sockaddr *) &cli_addr,
 	&clilen);
-    if (newsockfd < 0) 
+    if (newsockfd < 0)
       error("ERROR on accept");
-    //We have accepted a connection
-    bzero(buffer,256);
-    n = read(newsockfd,buffer,255);
-    if (n < 0) error("ERROR reading from socket");
-    printf("Here is the message: %s\n",buffer);
-    n = write(newsockfd,"I got your message",18);
-    if (n < 0) error("ERROR writing to socket");
-    close(newsockfd);
+
+    
+    pid_t spawnpid;
+
+    spawnpid = fork();
+
+    if(spawnpid == 0){  //If we are the parent, we want to continue
+
+/*
+      clilen = sizeof(cli_addr);
+      newsockfd = accept(sockfd, 
+	  (struct sockaddr *) &cli_addr, 
+	  &clilen);
+      if (newsockfd < 0) 
+	error("ERROR on accept");
+*/  
+      //We have accepted a connection
+      bzero(buffer,256);
+
+      //n = read(newsockfd,buffer,255);
+      //if (n < 0) error("ERROR reading from socket");
+      //printf("Here is the message: %s\n",buffer);
+
+      bzero(plaintext, 256);
+      n = read(newsockfd, plaintext, 255);
+      //printf("[Server: here is the plaintext: %s]\n", plaintext);
+
+      bzero(key, 256);
+      n = read(newsockfd, key, 255);
+      //printf("[Server: here is the key: %s]\n", key);
+
+      //Prep plaintext
+      int strLen = strlen(plaintext);
+      char * plaintextNumbers = malloc(sizeof(char) * strLen);
+      int i;
+
+      //printf("Server plaintext: ");
+
+      for(i = 0; i < strLen; ++i){
+	//printf("%d", (int) convert(plaintext[i]));
+	plaintextNumbers[i] = convert(plaintext[i]);
+      }
+
+      strLen = strlen(key);
+      char * keyNumbers = malloc(sizeof(char) * strLen);
+
+      //printf("\nServer key: ");
+
+      for(i = 0; i < strLen; ++i){
+	keyNumbers[i] = convert(key[i]);
+	//printf("%d ", (int) keyNumbers[i]);
+      }
+
+      //Process the encryption
+      char * encryptedNumbers = malloc(sizeof(char) * strLen);
+      //printf("\nServer encrypt: ");
+
+      for (i = 0; i < strLen; ++i){
+#ifdef ENCRYPT
+	encryptedNumbers[i] = (plaintextNumbers[i] + keyNumbers[i]) % 27;
+#endif
+#ifdef DECRYPT
+	encryptedNumbers[i] = (plaintextNumbers[i] - keyNumbers[i]) % 27;
+#endif
+	//printf("%d ", (int) encryptedNumbers[i]);
+      }
+
+      char * encrypted = malloc(sizeof(char) * strLen);
+      //printf("Server output: ");
+
+      for (i = 0; i < strLen; ++i){
+	//printf("%c", convert(encryptedNumbers[i]));
+	encrypted[i] = convert(encryptedNumbers[i]);
+      }
+
+
+      n = write(newsockfd, encrypted, strlen(encrypted));
+      if (n < 0) error("ERROR writing to socket");
+      close(newsockfd);
+
+      exit(0); //This should only ever be a spawned process, so it must die
+    }
   }while(1);
 
   close(sockfd);
   return 0; 
+}
+
+char convert(char input){
+  if(input > 30){ //Then we are being passed a letter
+    if(input == ' ')
+      return 26;
+    else {
+      return input - 65;
+    }
+  }
+  else{ //We are being passed a number
+    if(input == 26)
+      return ' ';
+    else {
+      return input + 65;
+    }
+  }
+
 }
